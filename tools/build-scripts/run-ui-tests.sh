e29fab84920dcaf594f7e7594169078f626cb22a
yuriysurzhikov <surzhickov.yura@yandex.ua>
2021-03-12 21:34:48 +0200

    create wipe data

--- tools/build-scripts/run-ui-tests.sh
+++ tools/build-scripts/run-ui-tests.sh
@@ -13,7 +13,7 @@
 #echo "no"
 
 #Start the emulator
-$ANDROID_HOME/tools/emulator @androidAVD -wipe-data &
+$ANDROID_HOME/tools/emulator -avd androidAVD &
 EMULATOR_PID=$!
 
 # Wait for Android to finish booting
