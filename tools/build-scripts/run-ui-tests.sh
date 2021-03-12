5fc44cec34a638627073db074d86305b78dd6320
Yurii Surzhykov <yurii.surzhykov@gcpsoftware.com>
2021-03-12 16:41:25 +0200

    created script for test run

---
+++ tools/build-scripts/run-ui-tests.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Build scripts for Jenkins
+#
+# (c) 2021 Global Consulting Partners
+#
+echo "============================== Script run-ui-tests.sh =============================="
+#Start the emulator
+$ANDROID_SDK/tools/emulator -avd Android28 -wipe-data &
+EMULATOR_PID=$!
+
+# Wait for Android to finish booting
+WAIT_CMD="$ANDROID_SDK/platform-tools/adb wait-for-device shell getprop init.svc.bootanim"
+until $WAIT_CMD | grep -m 1 stopped; do
+  echo "Waiting..."
+  sleep 1
+done
+
+# Unlock the Lock Screen
+$ANDROID_SDK/platform-tools/adb shell input keyevent 82
+
+# Clear and capture logcat
+$ANDROID_SDK/platform-tools/adb logcat -c
+$ANDROID_SDK/platform-tools/adb logcat > build/logcat.log &
+LOGCAT_PID=$!
+
+# Run the tests
+./gradlew connectedAndroidTest -i
+
+# Stop the background processes
+kill $LOGCAT_PID
+kill $EMULATOR_PID
