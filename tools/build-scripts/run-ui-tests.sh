2a9d52f89351913aba9d1755bc0710bd8fc869a8
yuriysurzhikov <surzhickov.yura@yandex.ua>
2021-03-12 19:44:28 +0200

    replaced path divider

--- tools/build-scripts/run-ui-tests.sh
+++ tools/build-scripts/run-ui-tests.sh
@@ -7,29 +7,29 @@
 echo "============================== Script run-ui-tests.sh =============================="
 
 #Download sdk image
-$ANDROID_HOME\\tools\\bin\\sdkmanager --install "system-images;android-25;google_apis;x86"
+$ANDROID_HOME/tools/bin/sdkmanager --install "system-images;android-25;google_apis;x86"
 
 #Creating emulator
-$ANDROID_HOME\\tools\\bin\\avdmanager create avd -n androidAVD -k "system-images;android-25;google_apis;x86" --force
+$ANDROID_HOME/tools/bin/avdmanager create avd -n androidAVD -k "system-images;android-25;google_apis;x86" --force
 echo "no"
 
 #Start the emulator
-$ANDROID_HOME\\tools\\emulator -avd androidAVD -s "768x1280" &
+$ANDROID_HOME/tools/emulator -avd androidAVD -s "768x1280" &
 EMULATOR_PID=$!
 
 # Wait for Android to finish booting
-WAIT_CMD="${ANDROID_HOME}\platform-tools\adb wait-for-device shell getprop init.svc.bootanim"
+WAIT_CMD="${ANDROID_HOME}/platform-tools/adb wait-for-device shell getprop init.svc.bootanim"
 until $WAIT_CMD | grep -m 1 stopped; do
   echo "Waiting..."
   sleep 1
 done
 
 # Unlock the Lock Screen
-$ANDROID_HOME\\platform-tools\\adb shell input keyevent 82
+$ANDROID_HOME/platform-tools/adb shell input keyevent 82
 
 # Clear and capture logcat
-$ANDROID_HOME\\platform-tools\\adb logcat -c
-$ANDROID_HOME\\platform-tools\\adb logcat > build/logcat.log &
+$ANDROID_HOME/platform-tools/adb logcat -c
+$ANDROID_HOME/platform-tools/adb logcat > build/logcat.log &
 LOGCAT_PID=$!
 
 # Run the tests
@@ -37,6 +37,6 @@ LOGCAT_PID=$!
 ./gradlew connectedAndroidTest -i
 
 # Stop the background processes
-$ANDROID_HOME\\platform-tools\\adb -e emu kill $LOGCAT_PID
-$ANDROID_HOME\\platform-tools\\adb -e emu kill $EMULATOR_PID
-$ANDROID_HOME\\tools\\bin\\avdmanager delete avd -n androidAVD
+$ANDROID_HOME/platform-tools/adb -e emu kill $LOGCAT_PID
+$ANDROID_HOME/platform-tools/adb -e emu kill $EMULATOR_PID
+$ANDROID_HOME/tools/bin/avdmanager delete avd -n androidAVD
